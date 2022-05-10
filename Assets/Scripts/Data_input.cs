2963f4f074075f85bc956428aeabf01a05e883bd
Ivanran225 <ivanr.an225@gmail.com>
2022-05-10 15:28:07 -0300

    v2

---
+++ Assets/Scripts/Data_input.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Data_input : MonoBehaviour
+{
+    public Text txtUserName;
+    // Start is called before the first frame update
+    public void ShowGreetings()
+    {
+        Debug.Log($"Hola {txtUserName.text}");
+    }
+}
